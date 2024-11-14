f8ceef369f78a83344f478564705284a77cbda2c
Michael Matus <michael@matus.at>
2024-11-14 22:04:00 +0100

    Add simple class tester
    A console app that searches for specified filter combinations is added to the solution.

---
+++ SearchFilterStack/Program.cs
@@ -0,0 +1,145 @@
+﻿using At.Matus.SchottFilter;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace SearchFilterStack
+{
+    class Program
+    {
+        static double minD = 1;
+        static double maxD = 3;
+        static double deltaD = 1;
+
+        static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            string workingDirectory = @"C:\Users\User\source\repos\At.Matus.SchottFilter\data";
+            //string workingDirectory = @"/Volumes/NO NAME/LaserModData/S01/T020S01.csv";
+            //string workingDirectory = Directory.GetCurrentDirectory();
+
+            SchottFilter[] collection = LoadFilters(workingDirectory);
+
+            FilterSpecification spec = new FilterSpecification();
+            spec.SetMinimumPermissibleSpectralTransmittance(340, 500, 0.35);
+            spec.SetMaximumPermissibleSpectralTransmittance(850, 1100, 0.01);
+
+            // single filter
+            ResultPod[] singleFilter = Try1Filter(collection, spec);
+            foreach (var f in singleFilter)
+            {
+                Console.WriteLine($"{f}");
+            }
+            Console.WriteLine();
+
+            // two filters
+            ResultPod[] twoFilters = Try2Filters(collection, spec);
+            foreach (var f in twoFilters)
+            {
+                Console.WriteLine($"{f}");
+            }
+            Console.WriteLine();
+
+            // three filters
+            ResultPod[] threeFilters = Try3Filters(collection, spec);
+            foreach (var f in threeFilters)
+            {
+                Console.WriteLine($"{f}");
+            }
+            Console.WriteLine();
+        }
+
+        //=========================================================================================//
+
+        static SchottFilter[] LoadFilters(string fromDirectory)
+        {
+            List<SchottFilter> filters = new List<SchottFilter>();
+            string[] filenames = Directory.GetFiles(fromDirectory, @"*.tra");
+            Array.Sort(filenames);
+            foreach (string fn in filenames)
+            {
+                filters.Add(new SchottFilter(fn));
+            }
+            return filters.ToArray();
+        }
+
+        static ResultPod[] Try1Filter(SchottFilter[] filterSet, FilterSpecification spec)
+        {
+            List<ResultPod> cf = new List<ResultPod>();
+            for (int i = 0; i < filterSet.Length; i++)
+            {
+                for (double d1 = minD; d1 <= maxD; d1 += deltaD)
+                {
+                    var combination = FilterMath.Combine(filterSet[i], d1);
+                    if (spec.Conforms(combination))
+                    {
+                        cf.Add(new ResultPod(combination, spec.Fitness(combination), spec.AverageTransmission(combination), spec.AverageBlocking(combination)));
+                    }
+                }
+            }
+            cf.Sort();
+            cf.Reverse();
+            return cf.ToArray();
+        }
+
+        static ResultPod[] Try2Filters(SchottFilter[] filterSet, FilterSpecification spec)
+        {
+            List<ResultPod> cf = new List<ResultPod>();
+            for (int i = 0; i < filterSet.Length; i++)
+            {
+                for (int j = i + 1; j < filterSet.Length; j++)
+                {
+                    for (double d1 = minD; d1 <= maxD; d1 += deltaD)
+                    {
+                        for (double d2 = minD; d2 <= maxD; d2 += deltaD)
+                        {
+                            var combination = FilterMath.Combine(filterSet[i], d1, filterSet[j], d2);
+                            if (spec.Conforms(combination))
+                            {
+                                cf.Add(new ResultPod(combination, spec.Fitness(combination), spec.AverageTransmission(combination), spec.AverageBlocking(combination)));
+                            }
+                        }
+                    }
+                }
+            }
+            cf.Sort();
+            cf.Reverse();
+            return cf.ToArray();
+        }
+
+        static ResultPod[] Try3Filters(SchottFilter[] filterSet, FilterSpecification spec)
+        {
+            List<ResultPod> cf = new List<ResultPod>();
+            for (int i = 0; i < filterSet.Length; i++)
+            {
+                for (int j = i + 1; j < filterSet.Length; j++)
+                {
+                    for (int k = j + 1; k < filterSet.Length; k++)
+                    {
+                        for (double d1 = minD; d1 <= maxD; d1 += deltaD)
+                        {
+                            for (double d2 = minD; d2 <= maxD; d2 += deltaD)
+                            {
+                                for (double d3 = minD; d3 < maxD; d3 += deltaD)
+                                {
+                                    var combination = FilterMath.Combine(filterSet[i], d1, filterSet[j], d2, filterSet[k], d3);
+                                    if (spec.Conforms(combination))
+                                    {
+                                        cf.Add(new ResultPod(combination, spec.Fitness(combination), spec.AverageTransmission(combination), spec.AverageBlocking(combination)));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            cf.Sort();
+            cf.Reverse();
+            return cf.ToArray();
+        }
+
+    }
+
+}
---
+++ SearchFilterStack/ResultPod.cs
@@ -0,0 +1,34 @@
+﻿using At.Matus.SchottFilter;
+using System;
+
+namespace SearchFilterStack
+{
+    public class ResultPod : IComparable<ResultPod>
+    {
+        public SchottFilter FilterCombination { get; }
+        public string Designation => FilterCombination.Designation;
+        public double Fitness { get; }
+        public double TauPass { get; }
+        public double TauBlock { get; }
+
+        public ResultPod(SchottFilter filter,
+                         double fitness,
+                         double tauPass,
+                         double tauBlock)
+        {
+            FilterCombination = filter;
+            Fitness = fitness;
+            TauPass = tauPass;
+            TauBlock = tauBlock;
+        }
+
+        public override string ToString() => $"{Designation}  {Fitness:F0}  {TauPass * 100:F1}%  {TauBlock * 100:F3}%";
+
+        public int CompareTo(ResultPod other)
+        {
+            if (Fitness < other.Fitness) return -1;
+            if (Fitness > other.Fitness) return 1;
+            return 0;
+        }
+    }
+}
